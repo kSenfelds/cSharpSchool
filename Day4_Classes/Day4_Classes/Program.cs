f3becf70ce7d91efb2daf7f1deaa0b73d4af4c69
kSenfelds <kr.senfelds@gmail.com>
2020-09-03 19:30:04 +0300

    Day 4 project created

---
+++ Day4_Classes/Day4_Classes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day4_Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
