5705d031c6ffc8095fa99d89020e4796e45cdfd7
kSenfelds <kr.senfelds@gmail.com>
2020-09-03 19:46:17 +0300

    Update Program.cs

--- Day4_Classes/Day4_Classes/Program.cs
+++ Day4_Classes/Day4_Classes/Program.cs
@@ -6,7 +6,7 @@ namespace Day4_Classes
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           // Console.WriteLine("Hello World!");
         }
     }
 }
