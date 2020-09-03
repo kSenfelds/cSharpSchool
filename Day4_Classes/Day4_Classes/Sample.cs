59cd93a52f17a9e2d3b506ed24c2e5ec63d65807
kSenfelds <kr.senfelds@gmail.com>
2020-09-03 21:28:34 +0300

    Sample class added

---
+++ Day4_Classes/Day4_Classes/Sample.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day4_Classes
+{
+    class Sample
+    {
+        //private un public
+       
+        public static void TestPrint()
+        {
+            Console.WriteLine("Hello world");
+            PrivatePrint();
+        }
+        private static void PrivatePrint()
+        {
+            Console.WriteLine("Šo visi neredz");
+        }
+    }
+}
