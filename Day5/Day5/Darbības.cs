8841df1910ea5457187cd3fccc58f5a6b5b9b50e
kSenfelds <kr.senfelds@gmail.com>
2020-09-04 19:09:58 +0300

    Day5 + homework added

---
+++ Day5/Day5/Darbības.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day5
+{
+    class Darbības
+    {
+        public static double saskaitīšana(double number1, double number2)
+        {
+            double result = 0;
+            result = number1 + number2;
+            return result;
+        }
+        public static double atņemšana(double number1, double number2)
+        {
+            double result = 0;
+            result = number1 - number2;
+            return result;
+        }
+       public static double reizināšana(double number1, double number2)
+        {
+            double result = 0;
+            result = number1 * number2;
+            return result;
+        }
+       public static double dalīšana(double number1, double number2)
+        {
+            double result = 0;
+            result = number1 / number2;
+            return result;
+        }
+
+    }
+    
+
+}
---
+++ Day5/Day5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Day5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ievadi skaitli Nr1:");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ievadi skaitli Nr2:");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Izvēlies darbību - Saskaitīt (+), Atņemt (-), Reizināt (*) vai dalīt (/)");
+            string darb = Console.ReadLine();
+            double result = 0;
+            if (darb=="+")
+            {
+               result= Darbības.saskaitīšana(a, b);
+            }
+            else if (darb == "-")
+            {
+              result=  Darbības.atņemšana(a, b);
+            }
+            else if (darb == "*")
+            {
+              result =  Darbības.reizināšana(a, b);
+            }
+            else if (darb == "/")
+            {
+             result=   Darbības.dalīšana(a, b);
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
