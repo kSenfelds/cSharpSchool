161c4bbfe9e2fe5d32915d05817c60f6674cf9c3
kSenfelds <kr.senfelds@gmail.com>
2020-09-08 19:26:10 +0300

    Update Board.cs

--- TicTacToe/TicTacToe/Board.cs
+++ TicTacToe/TicTacToe/Board.cs
@@ -1,13 +1,10 @@
-﻿using Microsoft.VisualBasic.CompilerServices;
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System;
 
 namespace TicTacToe
 {
     class Board
     {
-        private static string[,] board = 
+        public static string[,] board =
         {
             {" ","|", " ", "|"," " },
             {"-","+","-", "+","-" },
@@ -19,15 +16,14 @@ namespace TicTacToe
         public static void printBoard()
         {
 
-               for (int i= 0; i<board.GetLength(0); i++)
-               {
-                   for (int j =0; j<board.GetLength(1);i++)
-                   {
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); i++)
+                {
                     Console.Write(board[i, j]); // Index out of bounds
-                   }
-                 Console.WriteLine();
-               }
-
+                }
+                Console.WriteLine();
+            }
 
         }
 
