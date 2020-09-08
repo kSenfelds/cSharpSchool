117e264cfb58dc2d5c2ded5c787de9b5eac28ffe
kSenfelds <kr.senfelds@gmail.com>
2020-09-08 19:28:29 +0300

    school

---
+++ Day7_Objects/Day7_Objects/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Day7_Objects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter student name");
+            String name = Console.ReadLine();
+            Console.WriteLine("Enter student surname");
+            String surname = Console.ReadLine();
+            Console.WriteLine("Enter course");
+            int year = Convert.ToInt32(Console.ReadLine());
+            Students stud1 = new Students(name,surname,year);
+            stud1.printInfo();
+        }
+    }
+}
---
+++ Day7_Objects/Day7_Objects/Students.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day7_Objects
+{
+    class Students
+    {
+        public String name {get; set;}
+        public String surname { get; set; }
+        public int year { get; set; }
+
+
+
+        public void printInfo()
+        {
+            Console.WriteLine(name + " " + surname + " " + year);
+        }
+        public Students(String name, String surname, int year)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.year = year;
+        }
+
+    }
+}
