6f953019d2a87bcf7d02f2b264680541b8792c3f
kSenfelds <kr.senfelds@gmail.com>
2020-09-08 21:19:10 +0300

    added task 2

--- Day7_Objects/Day7_Objects/Program.cs
+++ Day7_Objects/Day7_Objects/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Day7_Objects
 {
@@ -6,14 +7,45 @@ namespace Day7_Objects
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter student name");
-            String name = Console.ReadLine();
-            Console.WriteLine("Enter student surname");
-            String surname = Console.ReadLine();
-            Console.WriteLine("Enter course");
-            int year = Convert.ToInt32(Console.ReadLine());
-            Students stud1 = new Students(name,surname,year);
-            stud1.printInfo();
+            List<Students> lstOfStudents = new List<Students>();
+            string choice = "";
+           while (choice!="0") 
+            {
+                Console.WriteLine("1- Add student");
+                Console.WriteLine("2- Print list of students");
+                Console.WriteLine("0- Exit");
+                Console.WriteLine();
+                choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        Console.WriteLine("Enter name");
+                        string name = Console.ReadLine();
+                        Console.WriteLine("Enter lastname");
+                        string lastname = Console.ReadLine();
+                        Console.WriteLine("Enter course");
+                        int year = Convert.ToInt32(Console.ReadLine());
+                        lstOfStudents.Add(new Students(name, lastname, year));
+                        break;
+                    case "2":
+                        if (lstOfStudents.Count == 0) 
+                        {
+                            Console.WriteLine("List is empty");
+                            
+                        }
+                        else
+                        {
+                            for (int i =0;i<lstOfStudents.Count;i++)
+                            {
+                                lstOfStudents[i].printInfo();
+                                Console.WriteLine();
+                            }
+                        }
+                        break;
+                }
+
+            }
         }
     }
 }
--- Day7_Objects/Day7_Objects/Students.cs
+++ Day7_Objects/Day7_Objects/Students.cs
@@ -8,7 +8,9 @@ namespace Day7_Objects
     {
         public String name {get; set;}
         public String surname { get; set; }
+        private int _year;
         public int year { get; set; }
+       
 
 
 
