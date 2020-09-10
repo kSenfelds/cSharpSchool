47a177a6025dea284fb981da401fa7aebc4517dc
kSenfelds <kr.senfelds@gmail.com>
2020-09-10 21:48:53 +0300

    aaa

---
+++ Day8_FileIO/Day8_FileIO/FileOperationscs.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Day8_FileIO
+{
+    class FileOperationscs
+    {
+      public static void ReadFile()
+        {
+            try 
+            {
+                StreamReader sr = new StreamReader();
+                string line = sr.ReadLine();
+                Console.WriteLine(line);
+                sr.Close();
+            }
+            catch
+            {
+                Console.WriteLine("ERROR");
+            }
+            
+        }
+
+    }
+}
---
+++ Day8_FileIO/Day8_FileIO/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day8_FileIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileOperationscs.ReadFile();
+        }
+    }
+}
