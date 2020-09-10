cd2d6160387e8de208f8c92e4fe4d2de584ceac5
kSenfelds <kr.senfelds@gmail.com>
2020-09-10 19:00:38 +0300

    updated board (still incomplete)

--- TicTacToe/TicTacToe/Board.cs
+++ TicTacToe/TicTacToe/Board.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 
 namespace TicTacToe
 {
     class Board
     {
+      public static  List<int> playerMoves = new List<int>();
+      public static  List<int> cpuMoves = new List<int>();
         public static string[,] board =
         {
             {" ","|", " ", "|"," " },
@@ -25,6 +29,30 @@ namespace TicTacToe
                 Console.WriteLine();
             }
 
+        }
+        public static void startGame ()
+        {
+            int playerCounter = 0;
+            int cpuCounter = 0;
+            string symbol = "X";
+            while (true)
+            {
+            
+                    Console.WriteLine("Player move - (1-9)");
+                playerCounter = Convert.ToInt32(Console.ReadLine());
+                playerMoves.Add(playerCounter);
+                switch (playerCounter)
+                {
+                    case 1:
+                        board[0, 0] = symbol;
+                        break;
+
+                }
+                Console.WriteLine("CPU move - (1-9)");
+                cpuMoves.Add(Convert.ToInt32(Console.ReadLine()));
+
+
+            }
         }
 
     }
