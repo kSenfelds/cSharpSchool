dc8a314859470759d8c64e3cc97416483502f40a
kSenfelds <kr.senfelds@gmail.com>
2020-09-11 19:20:09 +0300

    day 9 task1

---
+++ Day9_Files/Day9_Files/FileOperations.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Day9_Files
+{
+    class FileOperations
+    {
+        public static String filename;
+        public static void Write()
+        {
+            
+            try
+            {
+                Console.WriteLine("Ievadiet faila nosaukumu :");
+                filename = Console.ReadLine().Trim();
+                StreamWriter sw = new StreamWriter(filename + ".txt");
+                sw.WriteLine("Hello");
+                sw.WriteLine("World!");
+
+                sw.Close();
+            }
+            catch
+            {
+                Console.WriteLine("Neizdevas ierakstit faila!");
+            }
+        }
+        public static void Read (string location)
+        {
+            try
+            {
+               
+                StreamReader sr = new StreamReader(filename+".txt");
+                
+                string line =sr.ReadLine();
+                while (line != null)
+                {
+                    Console.WriteLine(line);
+                    line = sr.ReadLine();
+                }
+                sr.Close();
+            }
+            catch
+            {
+                Console.WriteLine("Neizdevas nolasīt failu!");
+            }
+        }
+    }
+}
---
+++ Day9_Files/Day9_Files/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Day9_Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileOperations.Write();
+            FileOperations.Read(FileOperations.filename);
+        }
+    }
+}
