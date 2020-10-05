2a92f8bb74727db2368e64808f31f8d04238fffb
kSenfelds <kr.senfelds@gmail.com>
2020-10-05 16:53:24 +0300

    Need to modify checkWin, add win counters

---
+++ frameworktest/WindowsFormsApptest1/WindowsFormsApptest1/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace WindowsFormsApptest1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startGameButton = new System.Windows.Forms.Button();
+            this.gameBoard = new System.Windows.Forms.Panel();
+            this.btn3_3 = new System.Windows.Forms.Button();
+            this.btn3_2 = new System.Windows.Forms.Button();
+            this.btn3_1 = new System.Windows.Forms.Button();
+            this.btn2_3 = new System.Windows.Forms.Button();
+            this.btn2_2 = new System.Windows.Forms.Button();
+            this.btn2_1 = new System.Windows.Forms.Button();
+            this.btn1_3 = new System.Windows.Forms.Button();
+            this.btn1_2 = new System.Windows.Forms.Button();
+            this.btn1_1 = new System.Windows.Forms.Button();
+            this.ProgressLabel = new System.Windows.Forms.Label();
+            this.XwinLabel = new System.Windows.Forms.Label();
+            this.OwinLabel = new System.Windows.Forms.Label();
+            this.DrawLabel = new System.Windows.Forms.Label();
+            this.gameBoard.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // startGameButton
+            // 
+            this.startGameButton.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.startGameButton.Location = new System.Drawing.Point(316, 27);
+            this.startGameButton.Name = "startGameButton";
+            this.startGameButton.Size = new System.Drawing.Size(95, 34);
+            this.startGameButton.TabIndex = 0;
+            this.startGameButton.Text = "New Game";
+            this.startGameButton.UseVisualStyleBackColor = false;
+            this.startGameButton.Click += new System.EventHandler(this.startGameButton_Click);
+            // 
+            // gameBoard
+            // 
+            this.gameBoard.Controls.Add(this.btn3_3);
+            this.gameBoard.Controls.Add(this.btn3_2);
+            this.gameBoard.Controls.Add(this.btn3_1);
+            this.gameBoard.Controls.Add(this.btn2_3);
+            this.gameBoard.Controls.Add(this.btn2_2);
+            this.gameBoard.Controls.Add(this.btn2_1);
+            this.gameBoard.Controls.Add(this.btn1_3);
+            this.gameBoard.Controls.Add(this.btn1_2);
+            this.gameBoard.Controls.Add(this.btn1_1);
+            this.gameBoard.Location = new System.Drawing.Point(225, 81);
+            this.gameBoard.Name = "gameBoard";
+            this.gameBoard.Size = new System.Drawing.Size(272, 262);
+            this.gameBoard.TabIndex = 1;
+            // 
+            // btn3_3
+            // 
+            this.btn3_3.Location = new System.Drawing.Point(189, 182);
+            this.btn3_3.Name = "btn3_3";
+            this.btn3_3.Size = new System.Drawing.Size(80, 80);
+            this.btn3_3.TabIndex = 8;
+            this.btn3_3.Text = " ";
+            this.btn3_3.UseVisualStyleBackColor = true;
+            this.btn3_3.Click += new System.EventHandler(this.btn3_3_Click);
+            // 
+            // btn3_2
+            // 
+            this.btn3_2.Location = new System.Drawing.Point(99, 182);
+            this.btn3_2.Name = "btn3_2";
+            this.btn3_2.Size = new System.Drawing.Size(80, 80);
+            this.btn3_2.TabIndex = 7;
+            this.btn3_2.Text = " ";
+            this.btn3_2.UseVisualStyleBackColor = true;
+            this.btn3_2.Click += new System.EventHandler(this.btn3_2_Click);
+            // 
+            // btn3_1
+            // 
+            this.btn3_1.Location = new System.Drawing.Point(13, 182);
+            this.btn3_1.Name = "btn3_1";
+            this.btn3_1.Size = new System.Drawing.Size(80, 80);
+            this.btn3_1.TabIndex = 6;
+            this.btn3_1.Text = " ";
+            this.btn3_1.UseVisualStyleBackColor = true;
+            this.btn3_1.Click += new System.EventHandler(this.btn3_1_Click);
+            // 
+            // btn2_3
+            // 
+            this.btn2_3.Location = new System.Drawing.Point(192, 91);
+            this.btn2_3.Name = "btn2_3";
+            this.btn2_3.Size = new System.Drawing.Size(80, 80);
+            this.btn2_3.TabIndex = 5;
+            this.btn2_3.Text = " ";
+            this.btn2_3.UseVisualStyleBackColor = true;
+            this.btn2_3.Click += new System.EventHandler(this.btn2_3_Click);
+            // 
+            // btn2_2
+            // 
+            this.btn2_2.Location = new System.Drawing.Point(99, 91);
+            this.btn2_2.Name = "btn2_2";
+            this.btn2_2.Size = new System.Drawing.Size(80, 80);
+            this.btn2_2.TabIndex = 4;
+            this.btn2_2.Text = " ";
+            this.btn2_2.UseVisualStyleBackColor = true;
+            this.btn2_2.Click += new System.EventHandler(this.btn2_2_Click);
+            // 
+            // btn2_1
+            // 
+            this.btn2_1.Location = new System.Drawing.Point(13, 91);
+            this.btn2_1.Name = "btn2_1";
+            this.btn2_1.Size = new System.Drawing.Size(80, 80);
+            this.btn2_1.TabIndex = 3;
+            this.btn2_1.Text = " ";
+            this.btn2_1.UseVisualStyleBackColor = true;
+            this.btn2_1.Click += new System.EventHandler(this.btn2_1_Click);
+            // 
+            // btn1_3
+            // 
+            this.btn1_3.Location = new System.Drawing.Point(192, 3);
+            this.btn1_3.Name = "btn1_3";
+            this.btn1_3.Size = new System.Drawing.Size(80, 80);
+            this.btn1_3.TabIndex = 2;
+            this.btn1_3.Text = " ";
+            this.btn1_3.UseVisualStyleBackColor = true;
+            this.btn1_3.Click += new System.EventHandler(this.btn1_3_Click);
+            // 
+            // btn1_2
+            // 
+            this.btn1_2.Location = new System.Drawing.Point(99, 3);
+            this.btn1_2.Name = "btn1_2";
+            this.btn1_2.Size = new System.Drawing.Size(80, 80);
+            this.btn1_2.TabIndex = 1;
+            this.btn1_2.Text = " ";
+            this.btn1_2.UseVisualStyleBackColor = true;
+            this.btn1_2.Click += new System.EventHandler(this.btn1_2_Click);
+            // 
+            // btn1_1
+            // 
+            this.btn1_1.AccessibleName = "btn1_1";
+            this.btn1_1.Location = new System.Drawing.Point(13, 3);
+            this.btn1_1.Name = "btn1_1";
+            this.btn1_1.Size = new System.Drawing.Size(80, 80);
+            this.btn1_1.TabIndex = 0;
+            this.btn1_1.Text = " ";
+            this.btn1_1.UseVisualStyleBackColor = true;
+            this.btn1_1.Click += new System.EventHandler(this.btn1_1_Click);
+            // 
+            // ProgressLabel
+            // 
+            this.ProgressLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ProgressLabel.Font = new System.Drawing.Font("Times New Roman", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ProgressLabel.Location = new System.Drawing.Point(225, 362);
+            this.ProgressLabel.Name = "ProgressLabel";
+            this.ProgressLabel.Size = new System.Drawing.Size(272, 65);
+            this.ProgressLabel.TabIndex = 2;
+            this.ProgressLabel.Text = "Start the game :)";
+            this.ProgressLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // XwinLabel
+            // 
+            this.XwinLabel.Location = new System.Drawing.Point(560, 124);
+            this.XwinLabel.Name = "XwinLabel";
+            this.XwinLabel.Size = new System.Drawing.Size(138, 40);
+            this.XwinLabel.TabIndex = 3;
+            this.XwinLabel.Text = "X wins = ";
+            // 
+            // OwinLabel
+            // 
+            this.OwinLabel.Location = new System.Drawing.Point(560, 172);
+            this.OwinLabel.Name = "OwinLabel";
+            this.OwinLabel.Size = new System.Drawing.Size(138, 40);
+            this.OwinLabel.TabIndex = 4;
+            this.OwinLabel.Text = "O wins = ";
+            // 
+            // DrawLabel
+            // 
+            this.DrawLabel.Location = new System.Drawing.Point(560, 297);
+            this.DrawLabel.Name = "DrawLabel";
+            this.DrawLabel.Size = new System.Drawing.Size(138, 40);
+            this.DrawLabel.TabIndex = 5;
+            this.DrawLabel.Text = "Draws = ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.DrawLabel);
+            this.Controls.Add(this.OwinLabel);
+            this.Controls.Add(this.XwinLabel);
+            this.Controls.Add(this.ProgressLabel);
+            this.Controls.Add(this.gameBoard);
+            this.Controls.Add(this.startGameButton);
+            this.Name = "Form1";
+            this.Text = "Tic-Tac-Toe";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.gameBoard.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startGameButton;
+        private System.Windows.Forms.Panel gameBoard;
+        private System.Windows.Forms.Button btn1_2;
+        private System.Windows.Forms.Button btn1_1;
+        private System.Windows.Forms.Button btn3_3;
+        private System.Windows.Forms.Button btn3_2;
+        private System.Windows.Forms.Button btn3_1;
+        private System.Windows.Forms.Button btn2_3;
+        private System.Windows.Forms.Button btn2_2;
+        private System.Windows.Forms.Button btn2_1;
+        private System.Windows.Forms.Button btn1_3;
+        private System.Windows.Forms.Label ProgressLabel;
+        private System.Windows.Forms.Label XwinLabel;
+        private System.Windows.Forms.Label OwinLabel;
+        private System.Windows.Forms.Label DrawLabel;
+    }
+}
+
---
+++ frameworktest/WindowsFormsApptest1/WindowsFormsApptest1/Form1.cs
@@ -0,0 +1,340 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApptest1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private static List<int> player1Moves = new List<int> ();
+        private static List<int> player2Moves = new List<int>();
+        private static List<int> cpuMoves = new List<int>();
+        string player = "player1";
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void startGameButton_Click(object sender, EventArgs e)
+        {
+           foreach (Control c in gameBoard.Controls)
+            {
+                if (c is Button)
+                {
+                    c.Text = "";
+                    c.Enabled = true;
+
+                }
+            }
+            ProgressLabel.Text = "Game in process!";
+            player1Moves.Clear();
+            cpuMoves.Clear();
+        }
+
+
+            private void btn1_1_Click(object sender, EventArgs e)
+        {   
+            if (player == "player1")
+            {
+                move(player, btn1_1, 1);
+                checkWinner(player1Moves, cpuMoves);
+                player = "cpu";
+            }
+            else if (player == "cpu")
+            {
+                move(player, btn1_1, 1);
+                checkWinner(player1Moves, cpuMoves);
+                player = "player1";
+            }
+           
+        }
+        private void btn1_2_Click(object sender, EventArgs e)
+        {
+            if (player == "player1")
+            {
+                move(player, btn1_2, 2);
+                checkWinner(player1Moves, cpuMoves);
+                player = "cpu";
+            }
+            else if (player == "cpu")
+            {
+                move(player, btn1_2, 2);
+                checkWinner(player1Moves, cpuMoves);
+                player = "player1";
+            }
+        }
+        private void btn1_3_Click(object sender, EventArgs e)
+        {
+            if (player == "player1")
+            {
+                move(player, btn1_3, 3);
+                checkWinner(player1Moves, cpuMoves);
+                player = "cpu";
+            }
+            else if (player == "cpu")
+            {
+                move(player, btn1_3, 3);
+                checkWinner(player1Moves, cpuMoves);
+                player = "player1";
+            }
+        }
+        private void btn2_1_Click(object sender, EventArgs e)
+        {
+            if (player == "player1")
+            {
+                move(player, btn2_1, 4);
+                checkWinner(player1Moves, cpuMoves);
+                player = "cpu";
+            }
+            else if (player == "cpu")
+            {
+                move(player, btn2_1, 4);
+                checkWinner(player1Moves, cpuMoves);
+                player = "player1";
+            }
+        }
+        private void btn2_2_Click(object sender, EventArgs e)
+        {
+            if (player == "player1")
+            {
+                move(player, btn2_2, 5);
+                checkWinner(player1Moves, cpuMoves);
+                player = "cpu";
+            }
+            else if (player == "cpu")
+            {
+                move(player, btn2_2, 5);
+                checkWinner(player1Moves, cpuMoves);
+                player = "player1";
+            }
+        }
+        private void btn2_3_Click(object sender, EventArgs e)
+        {
+            if (player == "player1")
+            {
+                move(player, btn2_3, 6);
+                checkWinner(player1Moves, cpuMoves);
+                player = "cpu";
+            }
+            else if (player == "cpu")
+            {
+                move(player, btn2_3, 6);
+                checkWinner(player1Moves, cpuMoves);
+                player = "player1";
+            }
+        }
+        private void btn3_1_Click(object sender, EventArgs e)
+        {
+            if (player == "player1")
+            {
+                move(player, btn3_1, 7);
+                checkWinner(player1Moves, cpuMoves);
+                player = "cpu";
+            }
+            else if (player == "cpu")
+            {
+                move(player, btn3_1, 7);
+                checkWinner(player1Moves, cpuMoves);
+                player = "player1";
+            }
+        }
+        private void btn3_2_Click(object sender, EventArgs e)
+        {
+            if (player == "player1")
+            {
+                move(player, btn3_2, 8);
+                checkWinner(player1Moves, cpuMoves);
+                player = "cpu";
+            }
+            else if (player == "cpu")
+            {
+                move(player, btn3_2, 8);
+                checkWinner(player1Moves, cpuMoves);
+                player = "player1";
+            }
+        }
+        private void btn3_3_Click(object sender, EventArgs e)
+        {
+            if (player == "player1")
+            {
+                move(player, btn3_3, 9);
+                checkWinner(player1Moves, cpuMoves);
+                player = "cpu";
+            }
+            else if (player == "cpu")
+            {
+                move(player, btn3_3, 9);
+                checkWinner(player1Moves, cpuMoves);
+                player = "player1";
+            }
+        }
+        private void move (string player, Button buttonName, int movePosition)
+        { String symbol = "X";
+            if (player == "player1")
+            {
+                symbol = "X";
+                buttonName.Text = symbol;
+                buttonName.Enabled = false;
+                switch (movePosition)
+                {
+                    case 1: player1Moves.Add(1);
+                        break;
+                    case 2:
+                        player1Moves.Add(2);
+                        break;
+                    case 3:
+                        player1Moves.Add(3);
+                        break;
+                    case 4:
+                        player1Moves.Add(4);
+                        break;
+                    case 5:
+                        player1Moves.Add(5);
+                        break;
+                    case 6:
+                        player1Moves.Add(6);
+                        break;
+                    case 7:
+                        player1Moves.Add(7);
+                        break;
+                    case 8:
+                        player1Moves.Add(8);
+                        break;
+                    case 9:
+                        player1Moves.Add(9);
+                        break;
+                }
+                symbol = "O";
+                
+               
+            }
+            else if (player == "cpu")
+            {
+                symbol = "O";
+                buttonName.Text = symbol;
+                buttonName.Enabled = false;
+                switch (movePosition)
+                {
+                    case 1:
+                        cpuMoves.Add(1);
+                        break;
+                    case 2:
+                        cpuMoves.Add(2);
+                        break;
+                    case 3:
+                        cpuMoves.Add(3);
+                        break;
+                    case 4:
+                        cpuMoves.Add(4);
+                        break;
+                    case 5:
+                        cpuMoves.Add(5);
+                        break;
+                    case 6:
+                        cpuMoves.Add(6);
+                        break;
+                    case 7:
+                        cpuMoves.Add(7);
+                        break;
+                    case 8:
+                        cpuMoves.Add(8);
+                        break;
+                    case 9:
+                        cpuMoves.Add(9);
+                        break;
+                }
+                symbol = "X";
+            }
+        }
+        private void checkWinner (List<int> player1Moves, List<int> cpuMoves)
+        { 
+            if (player1Moves.Contains(1) && player1Moves.Contains(2) && player1Moves.Contains(3) && btn1_1.Text == "X")
+            {
+                ProgressLabel.Text = "Player X wins";
+            }
+            else if (player1Moves.Contains(4) && player1Moves.Contains(5) && player1Moves.Contains(6) && btn2_1.Text == "X")
+            {
+                ProgressLabel.Text = "Player X wins";
+            }
+            else if (player1Moves.Contains(7) && player1Moves.Contains(8) && player1Moves.Contains(9) && btn3_1.Text == "X")
+            {
+                ProgressLabel.Text = "Player X wins";
+            }
+            else if (player1Moves.Contains(1) && player1Moves.Contains(4) && player1Moves.Contains(7) && btn1_1.Text == "X")
+            {
+                ProgressLabel.Text = "Player X wins";
+            }
+            else if (player1Moves.Contains(2) && player1Moves.Contains(5) && player1Moves.Contains(8) && btn1_2.Text == "X")
+            {
+                ProgressLabel.Text = "Player X wins";
+            }
+            else if (player1Moves.Contains(3) && player1Moves.Contains(6) && player1Moves.Contains(9) && btn1_3.Text == "X")
+            {
+                ProgressLabel.Text = "Player X wins";
+            }
+            else if (player1Moves.Contains(1) && player1Moves.Contains(5) && player1Moves.Contains(9) && btn1_1.Text == "X")
+            {
+                ProgressLabel.Text = "Player X wins";
+            }
+            else if (player1Moves.Contains(3) && player1Moves.Contains(5) && player1Moves.Contains(7) && btn1_3.Text == "X")
+            {
+                ProgressLabel.Text = "Player X wins";
+            }
+            else if (cpuMoves.Contains(1) && player1Moves.Contains(2) && player1Moves.Contains(3) && btn1_1.Text == "O")
+            {
+                ProgressLabel.Text = "Player O wins";
+            }
+            else if (cpuMoves.Contains(4) && player1Moves.Contains(5) && player1Moves.Contains(6) && btn2_1.Text == "O")
+            {
+                ProgressLabel.Text = "Player O wins";
+            }
+            else if (cpuMoves.Contains(7) && player1Moves.Contains(8) && player1Moves.Contains(9) && btn3_1.Text == "O")
+            {
+                ProgressLabel.Text = "Player O wins";
+            }
+            else if (cpuMoves.Contains(1) && player1Moves.Contains(4) && player1Moves.Contains(7) && btn1_1.Text == "O")
+            {
+                ProgressLabel.Text = "Player O wins";
+            }
+            else if (cpuMoves.Contains(2) && player1Moves.Contains(5) && player1Moves.Contains(8) && btn1_2.Text == "O")
+            {
+                ProgressLabel.Text = "Player O wins";
+            }
+            else if (cpuMoves.Contains(3) && player1Moves.Contains(6) && player1Moves.Contains(9) && btn1_3.Text == "O")
+            {
+                ProgressLabel.Text = "Player O wins";
+            }
+            else if (cpuMoves.Contains(1) && player1Moves.Contains(5) && player1Moves.Contains(9) && btn1_1.Text == "O")
+            {
+                ProgressLabel.Text = "Player O wins";
+            }
+            else if (cpuMoves.Contains(3) && player1Moves.Contains(5) && player1Moves.Contains(7) && btn1_3.Text == "O")
+            {
+                ProgressLabel.Text = "Player O wins";
+            }
+            else if (player1Moves.Count+cpuMoves.Count == 9)
+            {
+                ProgressLabel.Text = "TIE";
+            }
+            else
+            {
+                ProgressLabel.Text = "Game in process!";
+            }
+
+        }
+        
+
+        
+    }
+}
